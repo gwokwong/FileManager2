db7dea9a10e68d37df10d9d414c06bde8d623c23
wells <guowang1102@163.com>
2016-04-22 18:59:31 +0800

    file utils

--- app/src/main/java/com/wells/filemanager/MainActivity.java
+++ app/src/main/java/com/wells/filemanager/MainActivity.java
@@ -3,7 +3,6 @@ package com.wells.filemanager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
