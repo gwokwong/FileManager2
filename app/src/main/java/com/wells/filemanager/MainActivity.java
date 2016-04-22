877db620eca523aeb4714b5d4dcc22765504635e
wells <guowang1102@163.com>
2016-04-22 23:15:43 +0800

    添加更多静态方法

--- app/src/main/java/com/wells/filemanager/MainActivity.java
+++ app/src/main/java/com/wells/filemanager/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import java.io.File;
 
@@ -12,9 +11,8 @@ import java.io.File;
  * Created by wells on 16/4/21.
  */
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    private Button deleteBtn;
     private String sdAbsolutePath;
     private File sdFile;
 
@@ -22,8 +20,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        deleteBtn = (Button) findViewById(R.id.deleteBtn);
-        deleteBtn.setOnClickListener(this);
+    }
+
+    public void menuClick(View view){
+        switch (view.getId()){
+            case R.id.deleteBtn:
+                deleteSDEmptyDirectory();
+                break;
+            default:
+                break;
+        }
     }
 
     /**
@@ -34,17 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             sdAbsolutePath = Environment.getExternalStorageDirectory().getAbsolutePath();
             sdFile = new File(sdAbsolutePath);
             FileUtils.deleteEmptyDirectory(sdFile);
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.deleteBtn:
-                deleteSDEmptyDirectory();
-                break;
-            default:
-                break;
         }
     }
 }
