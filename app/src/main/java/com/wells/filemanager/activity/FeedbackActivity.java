6f894254a547708f0dc7f46ec004a66885a5a0cd
wells <guowang1102@163.com>
2016-04-29 17:53:30 +0800

    update ui

--- app/src/main/java/com/wells/filemanager/activity/FeedbackActivity.java
+++ app/src/main/java/com/wells/filemanager/activity/FeedbackActivity.java
@@ -35,77 +35,77 @@ public class FeedbackActivity extends TActivity {
     }
 
     private void initViews(){
-        nameEt = (EditText)findViewById(R.id.feedback_name);
-        callNumEt = (EditText)findViewById(R.id.feedback_call_num);
-        suggestionEt = (EditText)findViewById(R.id.feedback_suggestion);
-        calltypeRg = (RadioGroup)findViewById(R.id.feedback_call_type_rg);
-        QQRb = (RadioButton)findViewById(R.id.feedback_qq);
-        EmailRb = (RadioButton)findViewById(R.id.feedback_email);
-        phoneRb = (RadioButton)findViewById(R.id.feedback_phone);
-        weChatRb = (RadioButton)findViewById(R.id.feedback_wechat);
-        submitBtn = (Button)findViewById(R.id.feedback_submit);
-
-        calltypeRg.check(QQRb.getId());
-
-        showKeyboardDelayed(nameEt);
-
-        calltypeRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                int radioButtonId = radioGroup.getCheckedRadioButtonId();
-                switch (radioButtonId){
-                    case R.id.feedback_qq:
-                        callType = Feedback.CALL_TYPE_QQ;
-                        break;
-                    case R.id.feedback_email:
-                        callType =Feedback.CALL_TYPE_EMAIL;
-                        break;
-                    case R.id.feedback_phone:
-                        callType =Feedback.CALL_TYPE_PHONE;
-                        break;
-                    case R.id.feedback_wechat:
-                        callType =Feedback.CALL_TYPE_WECHAT;
-                        break;
-                    default:
-                        break;
-
-                }
-            }
-        });
-
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(TextUtils.isEmpty(suggestionEt.getText().toString())){
-                    toast("意见为空,请输入您的意见或者建议后再提交");
-                    return;
-                }
-
-                Feedback feedback = new Feedback();
-                feedback.setCallNum(callNumEt.getText().toString());
-                feedback.setCallType(callType);
-                feedback.setMsg(suggestionEt.getText().toString());
-                feedback.setName(nameEt.getText().toString());
-
-                feedback.save(FeedbackActivity.this, new SaveListener() {
-                    @Override
-                    public void onSuccess() {
-                        toast("感谢您的提交,我们会尽快查看您的建议和意见");
-                        //清空操作 or add 延时退出当前界面
-                        nameEt.setText("");
-                        callNumEt.setText("");
-                        suggestionEt.setText("");
-                        calltypeRg.check(QQRb.getId());
-                    }
-
-                    @Override
-                    public void onFailure(int i, String s) {
-                        toast("提交失败");
-
-                    }
-                });
-
-            }
-        });
+//        nameEt = (EditText)findViewById(R.id.feedback_name);
+//        callNumEt = (EditText)findViewById(R.id.feedback_call_num);
+//        suggestionEt = (EditText)findViewById(R.id.feedback_suggestion);
+//        calltypeRg = (RadioGroup)findViewById(R.id.feedback_call_type_rg);
+//        QQRb = (RadioButton)findViewById(R.id.feedback_qq);
+//        EmailRb = (RadioButton)findViewById(R.id.feedback_email);
+//        phoneRb = (RadioButton)findViewById(R.id.feedback_phone);
+//        weChatRb = (RadioButton)findViewById(R.id.feedback_wechat);
+//        submitBtn = (Button)findViewById(R.id.feedback_submit);
+//
+//        calltypeRg.check(QQRb.getId());
+//
+//        showKeyboardDelayed(nameEt);
+//
+//        calltypeRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+//                int radioButtonId = radioGroup.getCheckedRadioButtonId();
+//                switch (radioButtonId){
+//                    case R.id.feedback_qq:
+//                        callType = Feedback.CALL_TYPE_QQ;
+//                        break;
+//                    case R.id.feedback_email:
+//                        callType =Feedback.CALL_TYPE_EMAIL;
+//                        break;
+//                    case R.id.feedback_phone:
+//                        callType =Feedback.CALL_TYPE_PHONE;
+//                        break;
+//                    case R.id.feedback_wechat:
+//                        callType =Feedback.CALL_TYPE_WECHAT;
+//                        break;
+//                    default:
+//                        break;
+//
+//                }
+//            }
+//        });
+//
+//        submitBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(TextUtils.isEmpty(suggestionEt.getText().toString())){
+//                    toast("意见为空,请输入您的意见或者建议后再提交");
+//                    return;
+//                }
+//
+//                Feedback feedback = new Feedback();
+//                feedback.setCallNum(callNumEt.getText().toString());
+//                feedback.setCallType(callType);
+//                feedback.setMsg(suggestionEt.getText().toString());
+//                feedback.setName(nameEt.getText().toString());
+//
+//                feedback.save(FeedbackActivity.this, new SaveListener() {
+//                    @Override
+//                    public void onSuccess() {
+//                        toast("感谢您的提交,我们会尽快查看您的建议和意见");
+//                        //清空操作 or add 延时退出当前界面
+//                        nameEt.setText("");
+//                        callNumEt.setText("");
+//                        suggestionEt.setText("");
+//                        calltypeRg.check(QQRb.getId());
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int i, String s) {
+//                        toast("提交失败");
+//
+//                    }
+//                });
+//
+//            }
+//        });
     }
 }
